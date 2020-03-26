a51c1b4b966c9b9dc8bcacb0f3aa88e528d5c969
josh <jhell85@gmail.com>
2020-03-26 14:52:54 -0700

    added console logs for debugging

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,10 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
+		var operator = $("input:radio[name=operator]:checked").val();
+		console.log("1st number: " + number1);  // for debugging
+    console.log("2nd number: " + number2);  // for debugging
+    console.log("operator: " + operator);  // for debugging
     var result = add(number1, number2);
     $("#output").text(result);
   });

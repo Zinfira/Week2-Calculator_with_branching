cc932602bb6d220ce661350a101c4fa69c459c62
josh <jhell85@gmail.com>
2020-03-26 14:46:41 -0700

    Add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
-$(document).ready(function() {
-	console.log('JavaScipt is working');
+// business logic:
 
-	//on click function
-	$('form').submit(function(event) {
-		event.preventDefault();
-		console.log('click');
-	});
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+var subtract  = function(number1, number2) {
+	return number1 - number2;
+};
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
+
+
+// user interface logic:
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
 });
